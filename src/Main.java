156492c299dc83142ad982daaa8a2b777a1bd8ad
Ilya Gatilov <depoldominic@gmail.com>
2021-07-25 22:48:40 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int current_account = 100; // Начальная сумма счёта, в рублях
-        int replenishment_amount = 1100; // Сумма пополнения счёта, в рублях
+        int accountBalance = 100; // остаток на счёте, в рублях
+        int topUpBalance = 1100; // пополнение баланса, в рублях
         int bonus;
 
-        if (replenishment_amount > 1000){
-            bonus = replenishment_amount / 100;
+        if (topUpBalance > 1000) {
+            bonus = topUpBalance / 100;
         } else {
             bonus = 0;
         }
-        int final_invoice = current_account + replenishment_amount + bonus;
+        int totalBalance = accountBalance + topUpBalance + bonus;
         System.out.println("Бонусные рубли = " + bonus);
-        System.out.println("Итоговая сумма на счету клиента = " + final_invoice);
+        System.out.println("Итоговая сумма на счету клиента = " + totalBalance);
 
     }
 }
